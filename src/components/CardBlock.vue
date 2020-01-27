3123018199ada82e0289d9fca6e6d1ee5e689af0
Daulet Rakhmankul <daulet.rakhmankul@gmail.com>
2020-01-27 21:37:30 +0600

    CardBlock zhasalghan alghashqy commit

---
+++ src/components/CardBlock.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="" style="">
+        <div class="mb-3 h5 font-weight-bold">{{title}}</div>
+        <div class="mb-3 d-flex hide-scroll-bar">
+            <div class="p-1 p-md-2 mx-2 rounded text-nowrap" style="background-color: #e6e6e6" v-for="category in categories" v-bind:key="category.id">{{category.name}}</div>
+        </div>
+
+        <div class="d-flex hide-scroll-bar shadow-only-right" style="">
+            <div class="mr-4 border rounded" style="width: 200px; min-width: 200px;" v-for="organization in organizations" v-bind:key="organization.id">
+                <div class="px-1 py-1 text-center text-nowrap overflow-hidden" style="text-overflow: ellipsis;">{{organization.title}}</div>
+                <div style="height: 100px; max-height: 100px">
+                    <img v-bind:src="organization.img" alt="" class="img-fluid" />
+                </div>
+                <div class="px-1 hide-scroll-bar" style="height: 72px; overflow-y: auto; font-size: .85rem">
+                    {{organization.text}}
+                </div>
+                <div class="px-1 py-1 text-center overflow-hidden" style="text-overflow: ellipsis;">{{organization.footer}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CardBlock",
+        props: {
+            title: String,
+            categories: Array,
+            organizations: Array
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .shadow-left-right {
+        box-shadow: inset 25px 0 15px -25px rgba(0,0,0,0.45), inset -25px 0 15px -25px rgba(0,0,0,0.45);
+    }
+    .shadow-only-left {
+        box-shadow: inset 25px 0 15px -25px rgba(0,0,0,0.45)
+    }
+    .shadow-only-right {
+        box-shadow: inset -25px 0 15px -25px rgba(0,0,0,0.45);
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,163 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container-fluid px-0" style="max-width: 800px; font-family: 'Helvetica Neue',Arial,sans-serif; font-size: .95rem">
+        <div class="mb-4 border" style="height: 180px"></div>
+        <CardBlock class="mb-4" title="Услуги" v-bind:categories="service_categories" v-bind:organizations="services" />
+
+        <CardBlock class="mb-4" title="Еда" v-bind:categories="service_categories" v-bind:organizations="services" />
+
+        <CardBlock class="mb-4" title="Развлечение" v-bind:categories="service_categories" v-bind:organizations="services" />
+
+        <CardBlock class="mb-4" v-bind:title="rent_title" v-bind:categories="rent_categories" v-bind:organizations="rent_organizations" />
+
+        <div class="mb-4 border" style="height: 180px"></div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    // import HelloWorld from '@/components/HelloWorld.vue'
+    import CardBlock from '@/components/CardBlock.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        data: function() {
+            return {
+                service_categories: [
+                    {
+                        id: 1,
+                        name: 'Такси'
+                    },
+                    {
+                        id: 2,
+                        name: 'Еда'
+                    },
+                    {
+                        id: 3,
+                        name: 'Ремонт'
+                    },
+                    {
+                        id: 4,
+                        name: 'Развлечение'
+                    },
+                    {
+                        id: 5,
+                        name: 'Спорт'
+                    },
+                    {
+                        id: 6,
+                        name: 'Для авто'
+                    },
+                    {
+                        id: 7,
+                        name: 'Отдых'
+                    }
+                ],
+                services: [
+                    {
+                        id: 1,
+                        title: 'Кафе Интерстеллар',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Выгодные цены, хорошие удобство. График работы: круглосуточно. Вежливые | офицанты. Разнообразный меню. Деликатесы.',
+                        footer: '8-747-112-63-03'
+                    },
+                    {
+                        id: 2,
+                        title: 'Ресторан Три Звезды',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Посетите наш ресторан в любое время. Наш адрес г. Тараз, улица Мечнокова, 35 (Напротив Налогового)',
+                        footer: '8-747-112-63-03'
+                    },
+                    {
+                        id: 3,
+                        title: 'Пивной Бар Белая Медведица',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Круглосуточно работаем. Адрес: перекресток ул. Абая и Конаева. ',
+                        footer: '8-747-112-63-03'
+                    },
+                    {
+                        id: 4,
+                        title: 'Евро Столовое',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Комплексный обет от 550 тг. Акция: Купи чай один раз, а дальше бесплатно. Работаем с 9:00 до 22:00',
+                        footer: '8-747-112-63-03'
+                    },
+                    {
+                        id: 5,
+                        title: 'Кафе Компот',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Разнобразный меню и разные напитки. Только самые вкусные. Наш адрес мкр. Жансая торговый дом DavMIR',
+                        footer: '8-747-112-63-03'
+                    }
+                ],
+                rent_title: 'Посуточная аренда',
+                rent_categories: [
+                    {
+                        id: 1,
+                        name: 'Квартира'
+                    },
+                    {
+                        id: 4,
+                        name: 'Комната'
+                    },
+                    {
+                        id: 2,
+                        name: 'Хостел'
+                    },
+                    {
+                        id: 3,
+                        name: 'Коттедж'
+                    },
+                    {
+                        id: 5,
+                        name: 'Дом'
+                    }
+                ],
+                rent_organizations: [
+                    {
+                        id: 1,
+                        title: 'Ночовка',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Посуточная аренда комнаты 3000 тг/ночь, 30% скидка если арендуете на неделю',
+                        footer: '@instagram.com/myaccount_official'
+                    },
+                    {
+                        id: 2,
+                        title: 'Хостел Даулет',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Койка места 5000 тг/сутка. Обед и ужин с нас. Поставим коммандировочные, есть ИП.',
+                        footer: '8-747-112-63-03'
+                    },
+                    {
+                        id: 3,
+                        title: 'Хостел Даулет',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Койка места 5000 тг/сутка. Обед и ужин с нас. Поставим коммандировочные, есть ИП.',
+                        footer: '8-747-112-63-03'
+                    },
+                    {
+                        id: 4,
+                        title: 'Хостел Даулет',
+                        img: 'https://via.placeholder.com/200x100',
+                        text: 'Койка места 5000 тг/сутка. Обед и ужин с нас. Поставим коммандировочные, есть ИП.',
+                        footer: '8-747-112-63-03'
+                    }
+                ]
+            }
+        },
+        components: {
+            CardBlock
+        }
+    }
 </script>
+
+<style>
+    .hide-scroll-bar {
+        overflow-x: scroll;
+        scrollbar-width: none; /* Firefox */
+        -ms-overflow-style: none;  /* Internet Explorer 10+ */
+    }
+    .hide-scroll-bar::-webkit-scrollbar { /* WebKit */
+        width: 0;
+        height: 0;
+    }
+</style>
